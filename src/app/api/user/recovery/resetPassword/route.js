8d9344b073ca417bb6f36de3494129b81d6dd354
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-06-19 07:33:22 +0600

    feat: password reset

---
+++ src/app/api/user/recovery/resetPassword/route.js
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+})
+
+export async function POST(req, res) {
+    try {
+        let reqBody = await req.json();
+        // user count
+        const count = await prisma.users.count(
+            {
+                where: {
+                    email: reqBody['email'],
+                    otp: reqBody['otp']
+                }
+            }
+        );
+        if(count === 1) {
+            const new_raw_pass = reqBody['password'];
+            const hashedPassword = await bcrypt.hash(new_raw_pass, 10);
+             const user = await prisma.users.update({
+                where: {
+                    email: reqBody['email'], // 👈 Ensure this is unique or use `id`
+                },
+                data: {
+                    password: hashedPassword, // Replace plain text password
+                }
+            });
+            const { password, ...userWithoutPassword } = user;
+            return NextResponse.json({ status: "success", data: userWithoutPassword });
+        } else {
+            return NextResponse.json({
+                 status: "fail", error: `Invalid OTP code` },
+                  { status: 400 }
+            );
+        }
+    } catch (error) {
+        return NextResponse.json(
+            { status: "fail", error: error.message },
+            { status: 400 }
+        );
+    }
+}
